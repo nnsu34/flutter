066c49f1eb7c385c0db346efe214537aefe1b9cc
nnsu46 <nnsu46@gmail.com>
2024-08-22 14:10:54 +0900

    INIT

---
+++ hello_world/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Text(
+            'Hello World',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
