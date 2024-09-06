ba878a52b257baa9f98a6ee9ec12e12671040642
nnsu34 <thsxhq34@gmail.com>
2024-09-06 09:18:53 +0900

    INIT

--- padding/lib/screen/home_screen.dart
+++ padding/lib/screen/home_screen.dart
@@ -6,24 +6,23 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          color: Colors.red,
-          child: Padding(
-            padding: EdgeInsets.fromLTRB(
-              32.0,
-              64.0,
-              16.0,
-              8.0,
-            ),
-            child: Container(
-              color: Colors.blue,
-              width: 50.0,
-              height: 50.0,
-            ),
+        body: Center(
+      child: Container(
+        color: Colors.red,
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(
+            32,
+            64,
+            16,
+            8,
+          ),
+          child: Container(
+            color: Colors.blue,
+            width: 50,
+            height: 50,
           ),
         ),
       ),
-    );
+    ));
   }
 }
