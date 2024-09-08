6ab66d05b677f0fe51ea38e86db86e8adf886f46
nnsu34 <thsxhq34@gmail.com>
2024-09-08 10:35:17 +0900

    INIT

--- project/image_carousel/test/widget_test.dart
+++ project/image_carousel/test/widget_test.dart
@@ -14,7 +14,7 @@ import 'package:image_carousel/screen/home_screen.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const HomeScreen());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
