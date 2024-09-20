fdc738fbb026916b68d30f75397abebc552ecf58
nnsu34 <thsxhq34@gmail.com>
2024-09-20 14:44:04 +0900

    INIT

---
+++ theory/statelesswidget_lifecycle/lib/screen/home_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TestWidget(),
+    );
+  }
+}
+
+class TestWidget extends StatelessWidget {
+  TestWidget({super.key}) {
+    print('--- 생성자 ---');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('--- 빌드 ---');
+    return Container(
+      width: 50,
+      height: 50,
+      color: Colors.red,
+    );
+  }
+}
