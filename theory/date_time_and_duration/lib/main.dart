bd33ece7a23aa61d0830ee7a691c8a6a12bd7898
nnsu34 <thsxhq34@gmail.com>
2024-09-24 11:32:42 +0900

    INIT

--- theory/date_time_and_duration/lib/main.dart
+++ theory/date_time_and_duration/lib/main.dart
@@ -63,7 +63,7 @@ void utc() {
 }
 
 void now() {
-  final now = DateTime.now();
+  final now = DateTime.now(); // 코드가 실행되는 순간의 날자와 시간을 알려준다.
 
   /// 현재 날짜와 시간이 출력됨
   print(now);
@@ -102,8 +102,8 @@ void dateCalc() {
     days: 1,
   );
 
-  print(date.add(duration));
-  print(date.subtract(duration));
+  print(date.add(duration)); //duration 기간만큼 기간을 늘릴 수 있다.
+  print(date.subtract(duration)); //duration 기간만큼 기간을 줄일 수 있다.
 }
 
 void comparison(){
@@ -119,12 +119,12 @@ void comparison(){
       23
   );
 
-  print(date1.isAfter(date2));
-  print(date1.isBefore(date2));
-  print(date1.isAtSameMomentAs(date2));
+  print(date1.isAfter(date2)); //이후의 날짜면 true 반환
+  print(date1.isBefore(date2)); // 이전의 날짜면  true 반환
+  print(date1.isAtSameMomentAs(date2)); // 같은 날짜면 true 반환 (년, 월, 날, 시간, 분, 초, 마이크로 초, 밀리초 모두 맞아야 함)
 }
 
-void toUtcToLocal(){
+void toUtcToLocal() {
   final date = DateTime(
       1992,
       11,
@@ -133,7 +133,13 @@ void toUtcToLocal(){
 
   final utcDate = date.toUtc();
 
+  /// 1992-11-23 00:00:00.000
   print(date);
+
+  /// 1992-11-22 15:00:00.000Z (Z가 끝에 있으면 UTC기준이라는 의미)
   print(utcDate);
-  print(utcDate.toLocal());
+
+  /// 1992-11-23 00:00:00.000
+  print(utcDate.toLocal()); // UTC 기준 날짜를 현지 날짜로 수정
+
 }
