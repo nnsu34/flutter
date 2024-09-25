9aee6f2d910c5912338a7c6a714163d6f0e31857
nnsu34 <thsxhq34@gmail.com>
2024-09-25 17:26:33 +0900

    INIT

--- project/u_and_i/lib/main.dart
+++ project/u_and_i/lib/main.dart
@@ -3,35 +3,31 @@ import 'package:u_and_i/screen/home_screen.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: 'sunflower',
-        textTheme: TextTheme(
-          displayLarge: TextStyle(
-            color: Colors.white,
-            fontSize: 80,
-            fontFamily: 'parisienne',
-          ),
-          displayMedium: TextStyle(
-            color: Colors.white,
-            fontFamily: 'sunflower',
-            fontSize: 50,
-            fontWeight: FontWeight.w700,
-          ),
-          bodyLarge: TextStyle(
-            color: Colors.white,
-            fontFamily: 'sunflower',
-            fontSize: 30,
-          ),
-          bodyMedium: TextStyle(
-            color: Colors.white,
-            fontFamily: 'sunflower',
-            fontSize: 20,
+      MaterialApp(
+        theme: ThemeData(
+          fontFamily: 'sunflower',
+          textTheme: TextTheme(
+            displayLarge: TextStyle(
+              color: Colors.white,
+              fontSize: 80.0,
+              fontFamily: 'parisienne',
+            ),
+            displayMedium: TextStyle(
+              color: Colors.white,
+              fontSize: 50.0,
+              fontWeight: FontWeight.w700,
+            ),
+            bodyLarge: TextStyle(
+              color: Colors.white,
+              fontSize: 30.0,
+            ),
+            bodyMedium: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+            ),
           ),
         ),
-      ),
-      home: HomeScreen(),
-    ),
+        home: HomeScreen(),
+      )
   );
 }
