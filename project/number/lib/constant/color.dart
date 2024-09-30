a4db6b22b70df59230253037bf285e2646b72e55
nnsu34 <thsxhq34@gmail.com>
2024-09-30 18:20:18 +0900

    INIT

--- project/number/lib/constant/color.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const primaryColor = Color(0xFF3D34B3); // 기본 배경색 (약간의 투명도 포함)
-const randomNumberBackgroundColor = Color(0xFF645BC6); // 랜덤 번호 배경색
-const selectedColor = Color(0xFFF85C60); // 강조된 색상
-const textColor = Color(0xFF4B4B6D); // 텍스트 색상
