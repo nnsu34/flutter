9f81f1bb611e74d7f2ecb694489f29e5996c482b
nnsu34 <thsxhq34@gmail.com>
2024-10-02 17:18:50 +0900

    INIT

--- project/random_number/lib/main.dart
+++ project/random_number/lib/main.dart
@@ -4,6 +4,21 @@ import 'package:number/screen/home_screen.dart';
 void main() {
   runApp(
     MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            fontFamily: 'safety',
+            fontSize: 28.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+          bodyMedium: TextStyle(
+            fontFamily: 'safety',
+            fontSize: 18.0,
+            color: Colors.white,
+          ),
+        ),
+      ),
       home: HomeScreen(),
       debugShowCheckedModeBanner: false,
     ),
